52c1ce8ff9deca04f5a2861b49189e332997fa84
Britzlieg <11417984781@qq.com>
2014-10-07 23:10:45 +0800

    调用api测试

--- weiboSDK/myWeibo.py
+++ weiboSDK/myWeibo.py
@@ -5,14 +5,14 @@ import urllib.request
 import json
 
 # 常数定义
-CLIENT_ID = '3104686382'
-CLIENT_SERCRET ='abf00732481e4bb63b9ea4288249ac29'
+CLIENT_ID = '1244782104'  #这个貌似未认证。。。,所以只能用自己的测试账号2390635102@qq.com 19920430
+CLIENT_SERCRET ='2482495e4fa525e21153b910c1febaa3'
 REDIRECT_URI ='http://www.baidu.com'
 BASEURL='https://api.weibo.com/2/'
 ACCESSTOKENURL='https://api.weibo.com/oauth2/access_token'
 AUTHORIZEURL='https://api.weibo.com/oauth2/authorize'
 RMURL='https://rm.api.weibo.com/2/'
-CODE = 'cfd66b9bb56655db80acc57a84a425cc'
+CODE = 'd0fad9aa9c8d69174ebcd98fba736136'
 access_token_file_path = 'token.txt'
 
 class APIClient:
@@ -25,7 +25,6 @@ class APIClient:
         token_data = json.loads(r_data)["access_token"]
         self.saveToken(token_data)
         print(r_data)
-
     def saveToken(self,token):
         f = open(access_token_file_path, 'w')
         f.write(token)
@@ -34,6 +33,18 @@ class APIClient:
         f = open(access_token_file_path, 'r')
         token = f.read()
         return token
-
+    # 获取最新的公共微博json字符串
+    def getPublicWeibo(self):
+        count = '200'
+        url = 'https://api.weibo.com/2/statuses/public_timeline.json'
+        targetUrl = '%s?%s=%s&&count=%s'%(url,'access_token',self.getToken(),count)
+        request = urllib.request.Request(targetUrl)
+        try:
+            d = urllib.request.urlopen(request)
+            data = d.read()
+            return data
+        except:
+            return '获取公共微博失败'
+        
 a = APIClient()
-print(a.getToken())
+
