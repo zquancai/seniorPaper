ed779ac905b5df319f69c3dcdf06014188b9207d
Britzlieg <11417984781@qq.com>
2015-02-01 20:17:50 +0800

    做好饼图和柱状图

--- show/src/linebar_chart.js
+++ show/src/linebar_chart.js
@@ -1,50 +1,53 @@
-// 路径配置
-require.config({
-    paths: {
-        echarts: '../echarts'
-    }
-});
+// 柱状图
+
+function myblock_lineBar(my_x,my_y,my_desc,myChart){
+    // 基于准备好的dom，初始化echarts图表
+
+    var option = {
+        tooltip: {
+            show: true
+        },
+        toolbox: {
+        show : true,
+        feature : {
+            mark : {show: true},
+            dataView : {show: true, readOnly: false},
+            magicType : {show: true, type: ['line', 'bar']},
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+        legend: {
+            data:my_desc,
+            
+        },
+        xAxis : [
+            {
+                type : 'category',
+                data : my_x
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                "name":my_desc,
+                "type":"bar",
+                "data":my_y
+            }
+        ]
+    };
+
+    // 为echarts对象加载数据 
+    myChart.setOption(option); 
+}
 
 
+function showLineBar(my_x,my_y,my_desc,myblock2_lineBar){
+    showChart(my_x,my_y,my_desc,myblock_lineBar,myblock2_lineBar,'LineBarChart');
+}
 
-// 使用
-require(
-    [
-        'echarts',
-        'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
-    ],
-    function (ec) {
-        // 基于准备好的dom，初始化echarts图表
-        var myChart = ec.init(document.getElementById('LineBarChart')); 
-        
-        var option = {
-            tooltip: {
-                show: true
-            },
-            legend: {
-                data:my_desc
-            },
-            xAxis : [
-                {
-                    type : 'category',
-                    data : my_x
-                }
-            ],
-            yAxis : [
-                {
-                    type : 'value'
-                }
-            ],
-            series : [
-                {
-                    "name":my_desc,
-                    "type":"bar",
-                    "data":my_y
-                }
-            ]
-        };
 
-        // 为echarts对象加载数据 
-        myChart.setOption(option); 
-    }
-);
--- show/src/value.js
+++ show/src/value.js
@@ -4,3 +4,28 @@
 var news_gender = new Array('男','女');
 var news_genderNum = new Array(100,222);
 var news_desc = new Array("人数");
+
+// 展示的函数模板
+function showChart(my_x,my_y,my_desc,myblock,myblock2,domId){
+    // 路径配置
+    require.config({
+        paths: {
+            echarts: '../echarts'
+        }
+    });
+
+    // 使用
+    require(
+        [
+            'echarts',
+            'echarts/chart/bar', // 使用柱状图就加载bar模块，按需加载
+            'echarts/chart/pie'
+        ],
+        function (ec) {
+            var myChart = ec.init(document.getElementById(domId)); 
+            myblock(my_x,my_y,my_desc,myChart);
+            myblock2(myChart);
+        }
+    );
+
+}
